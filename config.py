156b38d105e763212a134c8b30a95dee102952ec
mfelz <mattfelz@gmail.com>
2017-05-18 11:18:10 -0500

    fixed issues with ignore

--- config.py
+++
@@ -1,9 +0,0 @@
-owm_key =  "91338b1ef629e2fa338c5ea902ad181c"
-wu_key = "f7667b583545525c"
-
-#postgres://[username]:[password]@[host]:[port]/[database]
-db_username = "postgres"
-db_password = "sagence"
-db_host = "localhost"
-db_port = "5432"
-db_database = "weather"
